410274442c773425864812e249a64f4252d51002
Jinheng Zhu <jhzhuustc@gmail.com>
2022-12-04 13:41:49 +0800

    feature: add preview in design mode

--- src/AntdDateRangePicker.editorPreview.tsx
+++ src/AntdDateRangePicker.editorPreview.tsx
@@ -1,10 +1,22 @@
 import { Component, ReactNode, createElement } from "react";
-import { HelloWorldSample } from "./components/HelloWorldSample";
+import { DatePicker } from "antd";
+import { RangePickerProps } from "antd/es/date-picker";
 import { AntdDateRangePickerPreviewProps } from "../typings/AntdDateRangePickerProps";
+import "./ui/AntdDateRangePicker.scss";
 
+const { RangePicker } = DatePicker;
 export class preview extends Component<AntdDateRangePickerPreviewProps> {
     render(): ReactNode {
-        return <HelloWorldSample sampleText={""} />;
+        const props = this.props;
+        const pickerProps: RangePickerProps = {
+            picker: props.picker,
+            format: props.format,
+            size: props.size,
+            bordered: props.bordered,
+            className: props.class
+        };
+
+        return <RangePicker {...pickerProps} />;
     }
 }
 
